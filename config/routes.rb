d5962e617cec4c83989c42ddc29bc53aeedb9277
Yoke Yan Jocelyn Tan <jocelyn.tan.yoke@gmail.com>
2020-03-12 22:57:55 +1030

    Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :welcome
+  get '/quizzes', to: 'welcome#quizzes'
+  get '/podcasts', to: 'welcome#podcasts'
+  get '/bookreview', to: 'welcome#bookreview'
   get 'welcome/index'
   root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
