ce480ccd3ff70c2758ce573b8b331991142a765a
Yoke Yan Jocelyn Tan <jocelyn.tan.yoke@gmail.com>
2020-03-16 16:17:20 +1030

    Adding pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   resources :welcome
+  resources :people
+ 
+  post 'login', to: 'welcome#quizzes' 
   get '/quizzes', to: 'welcome#quizzes'
   get '/podcasts', to: 'welcome#podcasts'
   get '/bookreview', to: 'welcome#bookreview'
   get 'welcome/index'
   root 'welcome#index'
+  #root 'dashboard#index'
+ 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
